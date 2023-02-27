ee3d0e246673b7cfc2709ae9b67d7e5f6a73c859
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-02-27 16:49:17 +0900

    Support arm64 architecture also

--- packages/core/src/lib/nodeInfo.ts
+++ packages/core/src/lib/nodeInfo.ts
@@ -5,9 +5,13 @@ export const swankyNode = {
   polkadotPalletVersions: "polkadot-v0.9.37",
   supportedInk: "v4.0.0",
   downloadUrl: {
-    darwin:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-macOS-x86_64.tar.gz",
-    linux:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-ubuntu-x86_64.tar.gz",
+    darwin: {
+      x64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-macOS-x86_64.tar.gz",
+      arm64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-macOS-x86_64.tar.gz",
+    },
+    linux: {
+      x64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-ubuntu-x86_64.tar.gz",
+      arm64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-ubuntu-x86_64.tar.gz",
+    },
   },
 };
