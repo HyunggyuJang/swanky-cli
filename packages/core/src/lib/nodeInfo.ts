827129bcd71e93a0531146991b270bad722e4583
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-02-27 22:19:16 +0900

    Update download link

--- packages/core/src/lib/nodeInfo.ts
+++ packages/core/src/lib/nodeInfo.ts
@@ -6,12 +6,12 @@ export const swankyNode = {
   supportedInk: "v4.0.0",
   downloadUrl: {
     darwin: {
-      x64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-macOS-x86_64.tar.gz",
-      arm64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-macOS-x86_64.tar.gz",
+      x64: "https://github.com/HyunggyuJang/swanky-node/releases/download/v1.1.0-workshop/swanky-node-v1.1.0-workshop-macOS-universal.tar.gz",
+      arm64: "https://github.com/HyunggyuJang/swanky-node/releases/download/v1.1.0-workshop/swanky-node-v1.1.0-workshop-macOS-universal.tar.gz",
     },
     linux: {
-      x64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-ubuntu-x86_64.tar.gz",
-      arm64: "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-ubuntu-x86_64.tar.gz",
+      x64: "https://github.com/HyunggyuJang/swanky-node/releases/download/v1.1.0-workshop/swanky-node-v1.1.0-workshop-ubuntu-x86_64.tar.gz",
+      arm64: "https://github.com/HyunggyuJang/swanky-node/releases/download/v1.1.0-workshop/swanky-node-v1.1.0-workshop-ubuntu-aarch64.tar.gz",
     },
   },
 };
